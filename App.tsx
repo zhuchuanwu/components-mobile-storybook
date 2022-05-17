6f4beaa6b35aa4a843222d88eef52d4cdbdd9344
Jianlong Nie <1317272927@qq.com>
2022-05-17 17:50:01 +0800

    updates

--- App.tsx
+++ App.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import StorybookUIRoot from "./.ondevice/Storybook";
 import { Platform, StatusBar, View } from "react-native";
+import {
+  configureFonts,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import { color } from "react-native-tailwindcss";
+import * as Font from "expo-font";
 
-export default () => (
-  <View
-    style={{
-      flex: 1,
-      marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    }}
-  >
-    <StorybookUIRoot />
-  </View>
-);
+let theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: color.primary,
+    accent: "yellow",
+    text: color.black,
+    background: color.white,
+  },
+  fonts: configureFonts(fontConfig),
+};
+
+export default () => {
+  const [loaded, error] = Font.useFonts({
+    Mont: require("./assets/customFonts/Mont-Regular.ttf"),
+    MontBold: require("./assets/customFonts/Mont-SemiBold.ttf"),
+    MontBoldHeavy: require("./assets/customFonts/Mont-Bold.ttf"),
+  });
+  return  <PaperProvider theme={theme}>
+    <View>
+     <StorybookUIRoot />
+   </View>
+</PaperProvider>
+}
+ 
+
